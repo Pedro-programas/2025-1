3b935900241efe5d36fd4fcc3ae66bd477b64393
Pedro Henrique <pedroxphp@gmail.com>
2025-05-13 20:00:48 -0300

    questão1

--- q1/q1.c
+++ q1/q1.c
@@ -1,5 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool ehPrimo(int n){
+    if(n < 2) return false;
+        for(int i = 2; i*i <= n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+}
+
 
 int main(){
-    printf("Hello World");
+    int primeiro, ultimo;
+    printf("Digite o intervalo (dois números inteiros): ");
+    scanf("%d %d", &primeiro, &ultimo);
+
+    printf("Número primos entre %d e %d \n", primeiro, ultimo);
+
+    for(int i= primeiro; i <= ultimo; i++){
+        if(ehPrimo(i)){
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
 }
